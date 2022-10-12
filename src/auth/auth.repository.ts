722c42ad5a720ce09a938a29626855a73462ae65
Aakash Jha <aakash.jha1103@gmail.com>
2022-10-12 10:37:12 +0530

    Corrected DI implementation for authRepo
    - added mongoose as dependency for `auth.repository`
    - consuming authRepo in `auth.service`
    - manually tested `/register` endpoint with dummy data

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { AuthDocument } from './auth.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 class AuthRepository {
+  constructor(@InjectModel('auth') private authModel: Model<AuthDocument>) {}
+
   async findUser(email: string) {}
 
   async loginUser(email: string, password: string) {}
 
   async registerUser(email: string, password: string) {
-    return { user: { email, password }, success: true };
+    const _user = new this.authModel({
+      email,
+      password,
+    });
+    return await _user.save();
   }
 
   async generateApiKey(email: string) {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 
 import AuthRepository from './auth.repository';
-import { AuthDocument } from './auth.schema';
 
 @Injectable()
 class AuthService {
-  constructor(
-    public authRepo: AuthRepository,
-    @InjectModel('auth') private authModel: Model<AuthDocument>,
-  ) {}
+  constructor(public authRepo: AuthRepository) {}
 
   async loginService(email: string, password: string) {}
 
   async registerService(email: string, password: string) {
-    const newUser = new this.authModel({ email, password });
-    return await newUser.save();
+    return await this.authRepo.registerUser(email, password);
   }
 
   async generateApiKeyService() {}
