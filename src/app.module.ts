3d7741fa7e03ab56a8859284e59d001758d7aa11
Aakash Jha <aakash.jha1103@gmail.com>
2022-10-14 11:52:31 +0530

    Base for DMS module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 
 import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
+import { DmsModule } from './dms/dms.module';
 
 @Module({
   controllers: [AppController],
-  imports: [AuthModule, TasksModule, MongooseModule.forRoot(process.env.URI)],
+  imports: [AuthModule, TasksModule, MongooseModule.forRoot(process.env.URI), DmsModule],
 })
 export class AppModule {}
