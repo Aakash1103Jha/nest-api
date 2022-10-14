cd0e99e78a36b0a01c3e2be81d154c7b6dc6c36f
Aakash Jha <aakash.jha1103@gmail.com>
2022-10-14 14:47:45 +0530

    scaffold upload logic with s3

--- src/dms/dms.repository.ts
+++ src/dms/dms.repository.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { S3 } from 'aws-sdk';
 
-import { createReadStream } from 'fs';
-import { resolve } from 'path';
+import { createReadStream, readFileSync, statSync } from 'fs';
+import { resolve, extname } from 'path';
 
 const s3 = new S3({
   region: process.env.AWS_BUCKET_REGION,
@@ -12,12 +12,19 @@ export class DmsRepository {
   constructor() {}
 
   async uploadFile() {
-    const fileStream = createReadStream(resolve(__dirname, 'test.jpg'));
+    const filePath = resolve(__dirname, 'test2.jpg');
+    console.log(filePath);
+
+    const _file = statSync(filePath);
+    const fileStream = createReadStream(filePath);
+
     const UPLOAD_PARAMS: S3.PutObjectRequest = {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: Math.trunc(Math.random() * 1000000).toString(),
+      Key: `LynnieMarie/${Math.trunc(Math.random() * 1000000).toString()}`,
       Body: fileStream,
-      Metadata: { 'Content-Type': 'image/jpg' },
+      ContentType: `image/${extname(filePath).split('.')[1]}`,
+      ContentDisposition: 'inline',
+      ContentLength: _file.size,
     };
     const uploadResponse = s3.upload(UPLOAD_PARAMS).promise();
     return uploadResponse;
